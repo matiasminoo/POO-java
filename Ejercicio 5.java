0fb5ebf830ee259ffed75557d67c4de0860ca170
Matias miño <mati.mino2006@gmail.com>
2024-09-04 12:11:09 -0300

    Create Ejercicio 5.java

---
+++ Ejercicio 5.java
@@ -0,0 +1,10 @@
+public class nashe {
+	    public static void main(String[] args) {
+	        int numeros[] = {16,45,73,89,12};
+	        
+	        for (int numero : numeros) {
+	        	System.out.println(numero);
+	        }
+	    
+	    }
+	}
